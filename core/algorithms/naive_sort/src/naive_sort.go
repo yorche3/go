6fc36de1dde2a6c2870b742ff04da2835764d873
yorche3 <hyaoki123@gmail.com>
2026-09-15 21:19:39 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.go
+++ core/algorithms/naive_sort/src/naive_sort.go
@@ -11,3 +11,52 @@
 // Caso nulo: Go representa la entrada inválida con el slice `nil`, que se
 // retorna tal cual como indicador de fallo (equivalente al `int[]?` de C#).
 package src
+
+func SelectionSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	for i := 0; i < len(arr)-1; i++ {
+		minIdx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+	}
+	return arr
+}
+
+func BubbleSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				swapped = true
+			}
+		}
+	}
+	return arr
+}
+
+func InsertionSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+	return arr
+}
